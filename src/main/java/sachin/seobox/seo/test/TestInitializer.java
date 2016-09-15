a211f2c8946344d21638830683a90deb9e3e6021
Sachin <sachinkmr3112@gmail.com>
2016-09-15 11:15:28 +0530

    handling status codes

--- src/main/java/sachin/seobox/seo/test/TestInitializer.java
+++ src/main/java/sachin/seobox/seo/test/TestInitializer.java
@@ -1,8 +1,11 @@
 package sachin.seobox.seo.test;
 
 import java.io.File;
+import java.io.IOException;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.http.ParseException;
+import org.apache.http.client.ClientProtocolException;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
@@ -14,11 +17,26 @@ import edu.uci.ics.crawler4j.robotstxt.RobotstxtConfig;
 import edu.uci.ics.crawler4j.robotstxt.RobotstxtServer;
 import sachin.seobox.crawler.Crawler;
 import sachin.seobox.crawler.CrawlerConfig;
+import sachin.seobox.helpers.HelperUtils;
 
 public class TestInitializer {
 
 	@BeforeSuite(enabled = true)
 	public void init() {
+		try {
+			int code = HelperUtils.getUrlResponse(CrawlerConfig.site, CrawlerConfig.user, CrawlerConfig.pass)
+					.returnResponse().getStatusLine().getStatusCode();
+			if (code != 200) {
+				LoggerFactory.getLogger(CrawlerConfig.class).error("\n\nSite is giving " + code + " status code.\n\n");
+				System.exit(1);
+			}
+		} catch (ParseException e1) {
+			LoggerFactory.getLogger(CrawlerConfig.class).debug("Site is not running", e1);
+		} catch (ClientProtocolException e1) {
+			LoggerFactory.getLogger(CrawlerConfig.class).debug("Site is not running", e1);
+		} catch (IOException e1) {
+			LoggerFactory.getLogger(CrawlerConfig.class).debug("Site is not running", e1);
+		}
 		int numberOfCrawlers = Integer.parseInt(CrawlerConfig.PROPERTIES.getProperty("crawler.numberOfCrawlers", "30"));
 		CrawlerConfig control = new CrawlerConfig();
 		CrawlConfig config = control.getConfig();
