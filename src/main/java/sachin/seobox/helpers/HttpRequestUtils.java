50878a00d5a8444115dae8aa545cc094392810d1
Sachin <sachinkmr3112@gmail.com>
2016-10-03 12:23:53 +0530

    sitemap redir issue fix

--- src/main/java/sachin/seobox/helpers/HttpRequestUtils.java
+++ src/main/java/sachin/seobox/helpers/HttpRequestUtils.java
@@ -5,12 +5,16 @@ import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
 
+import org.apache.commons.codec.binary.Base64;
+import org.apache.http.ParseException;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.CredentialsProvider;
 import org.apache.http.client.config.CookieSpecs;
 import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.client.fluent.Response;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.protocol.HttpClientContext;
@@ -97,6 +101,31 @@ public class HttpRequestUtils {
 		HttpGet get = new HttpGet(add);
 		CloseableHttpResponse response = httpClient.execute(new HttpGet(add), context);
 		get.releaseConnection();
+		return response;
+	}
+
+	public static Response getUrlFluentResponse(String... data)
+			throws ParseException, ClientProtocolException, IOException {
+		String add = URLCanonicalizer.getCanonicalURL(data[0]);
+		Response response = null;
+		if (data.length == 1 || null == data[1] || data[1].trim().isEmpty()) {
+			response = Request.Get(add)
+					.connectTimeout(Integer
+							.parseInt(CrawlerConfig.PROPERTIES.getProperty("crawler.connectionTimeout", "20000")))
+					.socketTimeout(Integer
+							.parseInt(CrawlerConfig.PROPERTIES.getProperty("crawler.connectionTimeout", "20000")))
+					.execute();
+
+		} else {
+			String login = data[1] + ":" + data[2];
+			String base64login = new String(Base64.encodeBase64(login.getBytes()));
+			response = Request.Get(add).addHeader("Authorization", "Basic " + base64login)
+					.connectTimeout(Integer
+							.parseInt(CrawlerConfig.PROPERTIES.getProperty("crawler.connectionTimeout", "20000")))
+					.socketTimeout(Integer
+							.parseInt(CrawlerConfig.PROPERTIES.getProperty("crawler.connectionTimeout", "20000")))
+					.execute();
+		}
 		return response;
 	}
 }
