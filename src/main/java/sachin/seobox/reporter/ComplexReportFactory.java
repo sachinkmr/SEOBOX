986807ed26be5c2ccc13c2524d6758753b7f9847
Sachin <sachinkmr3112@gmail.com>
2016-10-14 09:58:44 +0530

    memory optimization

--- src/main/java/sachin/seobox/reporter/ComplexReportFactory.java
+++ src/main/java/sachin/seobox/reporter/ComplexReportFactory.java
@@ -1,8 +1,6 @@
 package sachin.seobox.reporter;
 
 import java.io.File;
-import java.util.HashMap;
-import java.util.Map;
 
 import com.relevantcodes.extentreports.DisplayOrder;
 import com.relevantcodes.extentreports.ExtentReports;
@@ -14,68 +12,61 @@ import sachin.seobox.common.SEOConfig;
 import sachin.seobox.helpers.HelperUtils;
 
 public class ComplexReportFactory {
-    private static ComplexReportFactory factory;
-    private ExtentReports reporter;
-    private Map<String, ExtentTest> nameToTestMap;
+	private static ComplexReportFactory factory;
+	private ExtentReports reporter;
+	// private Map<String, ExtentTest> nameToTestMap;
 
-    private ComplexReportFactory() {
-	nameToTestMap = new HashMap<>();
-	reporter = new ExtentReports(SEOConfig.reportPath, true, DisplayOrder.OLDEST_FIRST, NetworkMode.ONLINE);
-	reporter.loadConfig(new File(HelperUtils.getResourceFile("extent-config.xml")));
-    }
-
-    public synchronized static ComplexReportFactory getInstance() {
-	if (factory != null)
-	    return factory;
-	factory = new ComplexReportFactory();
-	return factory;
-    }
-
-    public ExtentTest getTest(String testName, String className, String testDescription) {
-	String name = className + "." + testName;
-	if (!nameToTestMap.containsKey(name)) {
-	    ExtentTest test = reporter.startTest(testName, testDescription);
-	    nameToTestMap.put(name, test);
+	private ComplexReportFactory() {
+		// nameToTestMap = new HashMap<>();
+		reporter = new ExtentReports(SEOConfig.reportPath, true, DisplayOrder.OLDEST_FIRST, NetworkMode.ONLINE);
+		reporter.loadConfig(new File(HelperUtils.getResourceFile("extent-config.xml")));
 	}
-	return nameToTestMap.get(name);
-    }
 
-    public ExtentReports getExtentReport() {
-	return reporter;
-    }
-
-    public ExtentTest getTest(String testName, String testDescription) {
-	if (!nameToTestMap.containsKey(testName)) {
-	    ExtentTest test = reporter.startTest(testName, testDescription);
-	    nameToTestMap.put(testName, test);
+	public synchronized static ComplexReportFactory getInstance() {
+		if (factory != null)
+			return factory;
+		factory = new ComplexReportFactory();
+		return factory;
 	}
-	return nameToTestMap.get(testName);
-    }
 
-    public ExtentTest getTest(String testName) {
-	return getTest(testName, "");
-    }
-
-    public void closeTest(String testName) {
-	if (!testName.isEmpty()) {
-	    ExtentTest test = getTest(testName);
-	    reporter.endTest(test);
+	public ExtentReports getExtentReport() {
+		return reporter;
 	}
-    }
 
-    public void closeTest(ExtentTest test) {
-	if (test != null) {
-	    test.log(LogStatus.INFO, "END", "Test Case Completed.");
-	    test.setEndedTime(HelperUtils.getTestCaseTime(System.currentTimeMillis()));
-	    reporter.endTest(test);
+	public ExtentTest getTest(String testName, String testDescription) {
+		return reporter.startTest(testName, testDescription);
+		// if (!nameToTestMap.containsKey(testName)) {
+		// ExtentTest test = reporter.startTest(testName, testDescription);
+		// nameToTestMap.put(testName, test);
+		// }
+		// return nameToTestMap.get(testName);
 	}
-    }
 
-    public void closeReport() {
-	if (reporter != null) {
-	    reporter.flush();
-	    reporter.close();
-	    reporter = null;
+	public ExtentTest getTest(String testName) {
+		return getTest(testName, "");
 	}
-    }
+
+	public void closeTest(String testName) {
+		if (!testName.isEmpty()) {
+			ExtentTest test = getTest(testName);
+			reporter.endTest(test);
+		}
+	}
+
+	public void closeTest(ExtentTest test) {
+		if (test != null) {
+			test.log(LogStatus.INFO, "END", "Test Case Completed.");
+			test.setEndedTime(HelperUtils.getTestCaseTime(System.currentTimeMillis()));
+			reporter.endTest(test);
+			reporter.flush();
+		}
+	}
+
+	public void closeReport() {
+		if (reporter != null) {
+			reporter.flush();
+			reporter.close();
+			reporter = null;
+		}
+	}
 }
