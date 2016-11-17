b1bdd9d34b62cbb35bfa4ba8e5ed36879e0b59b7
Sachin Kumar <sachinkmr3112@gmail.com>
2016-11-17 19:22:55 +0530

    test_id

--- src/main/java/sachin/seobox/reporter/ComplexReportFactory.java
+++ src/main/java/sachin/seobox/reporter/ComplexReportFactory.java
@@ -19,75 +19,75 @@ import sachin.seobox.common.SEOConfig;
 import sachin.seobox.helpers.HelperUtils;
 
 public class ComplexReportFactory {
-    private static ComplexReportFactory factory;
-    private ExtentReports reporter;
-    private SimpleDateFormat df = new SimpleDateFormat("h:mm:ss a");
-    private File tests = new File(new File(SEOConfig.dataLocation).getParentFile(), "tests");
-    private final MongoClient mongo;
+	private static ComplexReportFactory factory;
+	private ExtentReports reporter;
+	private SimpleDateFormat df = new SimpleDateFormat("h:mm:ss a");
+	private File tests = new File(new File(SEOConfig.dataLocation).getParentFile(), "tests");
+	private final MongoClient mongo;
 
-    private ComplexReportFactory() {
-	mongo = new MongoClient(SEOConfig.MONGODB_HOST, SEOConfig.MONGODB_PORT);
-	tests.mkdirs();
-	reporter = new ExtentReports(SEOConfig.reportPath, true, DisplayOrder.OLDEST_FIRST, NetworkMode.ONLINE);
-    }
-
-    public synchronized static ComplexReportFactory getInstance() {
-	if (factory != null)
-	    return factory;
-	factory = new ComplexReportFactory();
-	return factory;
-    }
-
-    public ExtentReports getExtentReport() {
-	return reporter;
-    }
-
-    public ExtentTest getTest(String testName, String testDescription) {
-	return reporter.startTest(testName, testDescription);
-    }
-
-    public ExtentTest getTest(String testName) {
-	return getTest(testName, "");
-    }
-
-    public void closeTest(String testName) {
-	if (!testName.isEmpty()) {
-	    ExtentTest test = getTest(testName);
-	    reporter.endTest(test);
+	private ComplexReportFactory() {
+		mongo = new MongoClient(SEOConfig.MONGODB_HOST, SEOConfig.MONGODB_PORT);
+		tests.mkdirs();
+		reporter = new ExtentReports(SEOConfig.reportPath, true, DisplayOrder.OLDEST_FIRST, NetworkMode.ONLINE);
 	}
-    }
 
-    public void closeTest(ExtentTest test) {
-	if (test != null) {
-	    test.log(LogStatus.INFO, "END", "Test Case Completed.");
-	    test.setEndedTime(HelperUtils.getTestCaseTime(System.currentTimeMillis()));
-	    reporter.endTest(test);
-	    Icon ic = new Icon();
-	    String id = test.getTest().getId().toString();
-	    for (Log log : test.getTest().getLogList()) {
-		Document arr = new Document("icon", ic.getIcon(log.getLogStatus()));
-		arr.append("status", log.getLogStatus().name());
-		arr.append("time", df.format(log.getTimestamp()));
-		arr.append("step", log.getStepName());
-		arr.append("detail", log.getDetails());
-		arr.append("id", id);
-		try {
-		    mongo.getDatabase("SEOBOX").getCollection(SEOConfig.REPORT_TIME_STAMP).insertOne(arr);
-		} catch (Exception ex) {
-		    LoggerFactory.getLogger(ComplexReportFactory.class).error("Error: " + ex);
+	public synchronized static ComplexReportFactory getInstance() {
+		if (factory != null)
+			return factory;
+		factory = new ComplexReportFactory();
+		return factory;
+	}
+
+	public ExtentReports getExtentReport() {
+		return reporter;
+	}
+
+	public ExtentTest getTest(String testName, String testDescription) {
+		return reporter.startTest(testName, testDescription);
+	}
+
+	public ExtentTest getTest(String testName) {
+		return getTest(testName, "");
+	}
+
+	public void closeTest(String testName) {
+		if (!testName.isEmpty()) {
+			ExtentTest test = getTest(testName);
+			reporter.endTest(test);
 		}
-	    }
-	    DashBoard.getInstance().addTest(test);
-	    test = null;
 	}
-    }
 
-    public void closeReport() {
-	mongo.close();
-	if (reporter != null) {
-	    reporter.close();
-	    reporter = null;
+	public void closeTest(ExtentTest test) {
+		if (test != null) {
+			test.log(LogStatus.INFO, "END", "Test Case Completed.");
+			test.setEndedTime(HelperUtils.getTestCaseTime(System.currentTimeMillis()));
+			reporter.endTest(test);
+			Icon ic = new Icon();
+			String id = test.getTest().getId().toString();
+			for (Log log : test.getTest().getLogList()) {
+				Document arr = new Document("icon", ic.getIcon(log.getLogStatus()));
+				arr.append("status", log.getLogStatus().name());
+				arr.append("time", df.format(log.getTimestamp()));
+				arr.append("step", log.getStepName());
+				arr.append("detail", log.getDetails());
+				arr.append("test_id", id);
+				try {
+					mongo.getDatabase("SEOBOX").getCollection(SEOConfig.REPORT_TIME_STAMP).insertOne(arr);
+				} catch (Exception ex) {
+					LoggerFactory.getLogger(ComplexReportFactory.class).error("Error: " + ex);
+				}
+			}
+			DashBoard.getInstance().addTest(test);
+			test = null;
+		}
 	}
-	factory = null;
-    }
+
+	public void closeReport() {
+		mongo.close();
+		if (reporter != null) {
+			reporter.close();
+			reporter = null;
+		}
+		factory = null;
+	}
 }
