eb7504918aabc7a2474ba342bb566251c4d7d907
Sachin <sachinkmr3112@gmail.com>
2017-02-07 12:42:34 +0530

    external link issue

--- src/main/java/sachin/seobox/helpers/DBUtils.java
+++ src/main/java/sachin/seobox/helpers/DBUtils.java
@@ -12,64 +12,66 @@ import sachin.seobox.exception.SEOException;
 import sachin.seobox.reporter.ComplexReportFactory;
 
 public class DBUtils {
-    protected static final Logger logger = LoggerFactory.getLogger(DBUtils.class);
+	protected static final Logger logger = LoggerFactory.getLogger(DBUtils.class);
 
-    public static void insertPageSpeedRecord(String url) {
-	JSONObject desktop = null;
-	JSONObject mobile = null;
-	Document arr = new Document("hasError", "false");
-	arr.append("url", url);
-	arr.append("page_speed_results", "true");
-	try {
-	    int i = 0;
-	    do {
-		desktop = HttpRequestUtils.getPageSpeedData(url, "desktop");
-		mobile = HttpRequestUtils.getPageSpeedData(url, "mobile");
-	    } while ((desktop == null || mobile == null) && ++i < 3);
-	    if (desktop != null) {
-		arr.append("desktop", desktop.toString());
-	    }
-	    if (mobile != null) {
-		arr.append("mobile", mobile.toString());
-	    }
-	} catch (SEOException e) {
-	    logger.debug("Unable to fatch page speed data", e);
-	    arr.append("hasError", "true");
-	    arr.append("error", e.toString());
+	public static void insertPageSpeedRecord(String url) {
+		JSONObject desktop = null;
+		JSONObject mobile = null;
+		Document arr = new Document("hasError", "false");
+		arr.append("url", url);
+		arr.append("page_speed_results", "true");
+		try {
+			int i = 0;
+			do {
+				desktop = HttpRequestUtils.getPageSpeedData(url, "desktop");
+				mobile = HttpRequestUtils.getPageSpeedData(url, "mobile");
+			} while ((desktop == null || mobile == null) && ++i < 3);
+			if (desktop != null) {
+				arr.append("desktop", desktop.toString());
+			}
+			if (mobile != null) {
+				arr.append("mobile", mobile.toString());
+			}
+		} catch (SEOException e) {
+			logger.debug("Unable to fatch page speed data", e);
+			arr.append("hasError", "true");
+			arr.append("error", e.toString());
+		}
+		ComplexReportFactory.getInstance().getMongoDB().getCollection(CrawlerConstants.REPORT_TIME_STAMP)
+				.insertOne(arr);
 	}
-	ComplexReportFactory.getInstance().getMongoDB().getCollection(CrawlerConstants.REPORT_TIME_STAMP).insertOne(arr);
-    }
 
-    public static JSONObject getPageSpeedRecord(String url) {
-	Document doc = ComplexReportFactory.getInstance().getMongoDB().getCollection(CrawlerConstants.REPORT_TIME_STAMP)
-		.findOneAndDelete(Filters.and(Filters.exists("page_speed_results"), Filters.eq("url", url)));
-	return new JSONObject(doc.toJson());
-    }
-
-    public static void insertStructuredDataRecord(String url, String html) {
-	JSONObject json = new JSONObject();
-	Document arr = new Document("hasError", "false");
-	arr.append("url", url);
-	arr.append("structured_data_results", "true");
-	try {
-	    int i = 0;
-	    do {
-		json = HttpRequestUtils.getStructuredData(html, url);
-	    } while ((json == null) && ++i < 3);
-	} catch (SEOException e) {
-	    logger.debug("Unable to fatch google structured data", e);
-	    arr.append("hasError", "true");
-	    arr.append("error", e.toString());
+	public static JSONObject getPageSpeedRecord(String url) {
+		Document doc = ComplexReportFactory.getInstance().getMongoDB().getCollection(CrawlerConstants.REPORT_TIME_STAMP)
+				.findOneAndDelete(Filters.and(Filters.exists("page_speed_results"), Filters.eq("url", url)));
+		return new JSONObject(doc.toString());
 	}
-	if (json != null) {
-	    arr.append("data", json.toString());
-	}
-	ComplexReportFactory.getInstance().getMongoDB().getCollection(CrawlerConstants.REPORT_TIME_STAMP).insertOne(arr);
-    }
 
-    public static JSONObject getStructuredDataRecord(String url) {
-	Document doc = ComplexReportFactory.getInstance().getMongoDB().getCollection(CrawlerConstants.REPORT_TIME_STAMP)
-		.findOneAndDelete(Filters.and(Filters.exists("structured_data_results"), Filters.eq("url", url)));
-	return new JSONObject(doc.toJson());
-    }
+	public static void insertStructuredDataRecord(String url, String html) {
+		JSONObject json = new JSONObject();
+		Document arr = new Document("hasError", "false");
+		arr.append("url", url);
+		arr.append("structured_data_results", "true");
+		try {
+			int i = 0;
+			do {
+				json = HttpRequestUtils.getStructuredData(html, url);
+			} while ((json == null) && ++i < 3);
+		} catch (SEOException e) {
+			logger.debug("Unable to fatch google structured data", e);
+			arr.append("hasError", "true");
+			arr.append("error", e.toString());
+		}
+		if (json != null) {
+			arr.append("data", json.toString());
+		}
+		ComplexReportFactory.getInstance().getMongoDB().getCollection(CrawlerConstants.REPORT_TIME_STAMP)
+				.insertOne(arr);
+	}
+
+	public static JSONObject getStructuredDataRecord(String url) {
+		Document doc = ComplexReportFactory.getInstance().getMongoDB().getCollection(CrawlerConstants.REPORT_TIME_STAMP)
+				.findOneAndDelete(Filters.and(Filters.exists("structured_data_results"), Filters.eq("url", url)));
+		return new JSONObject(doc.toJson());
+	}
 }
--- src/main/java/sachin/seobox/helpers/HttpRequestUtils.java
+++ src/main/java/sachin/seobox/helpers/HttpRequestUtils.java
@@ -5,6 +5,7 @@ import java.net.URISyntaxException;
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
 
 import javax.net.ssl.SSLContext;
 
@@ -45,168 +46,185 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import edu.uci.ics.crawler4j.url.URLCanonicalizer;
+import edu.uci.ics.crawler4j.url.WebURL;
+import edu.uci.ics.crawler4j.util.Util;
 import sachin.seobox.crawler.CrawlerConstants;
 import sachin.seobox.exception.SEOException;
 
 public class HttpRequestUtils {
-    protected static final Logger logger = LoggerFactory.getLogger(HttpRequestUtils.class);
+	protected static final Logger logger = LoggerFactory.getLogger(HttpRequestUtils.class);
 
-    /***
-     * 
-     * To get response for a url
-     * 
-     * @throws IOException
-     * @throws ClientProtocolException
-     * 
-     * 
-     */
+	/***
+	 * 
+	 * To get response for a url
+	 * 
+	 * @throws IOException
+	 * @throws ClientProtocolException
+	 * 
+	 * 
+	 */
 
-    @SuppressWarnings("deprecation")
-    public static CloseableHttpResponse getUrlResponse(String... data) throws ClientProtocolException, IOException {
-	String add = URLCanonicalizer.getCanonicalURL(data[0]);
+	@SuppressWarnings("deprecation")
+	public static CloseableHttpResponse getUrlResponse(String... data) throws ClientProtocolException, IOException {
+		String add = URLCanonicalizer.getCanonicalURL(data[0]);
 
-	RequestConfig requestConfig = RequestConfig.custom().setExpectContinueEnabled(false)
-		.setCookieSpec(CookieSpecs.IGNORE_COOKIES).setRedirectsEnabled(false)
-		.setSocketTimeout(
-			Integer.parseInt(CrawlerConstants.PROPERTIES.getProperty("crawler.connectionTimeout", "120000")))
-		.setConnectTimeout(
-			Integer.parseInt(CrawlerConstants.PROPERTIES.getProperty("crawler.connectionTimeout", "120000")))
-		.build();
+		RequestConfig requestConfig = RequestConfig.custom().setExpectContinueEnabled(false)
+				.setCookieSpec(CookieSpecs.IGNORE_COOKIES).setRedirectsEnabled(false)
+				.setSocketTimeout(Integer
+						.parseInt(CrawlerConstants.PROPERTIES.getProperty("crawler.connectionTimeout", "120000")))
+				.setConnectTimeout(Integer
+						.parseInt(CrawlerConstants.PROPERTIES.getProperty("crawler.connectionTimeout", "120000")))
+				.build();
 
-	RegistryBuilder<ConnectionSocketFactory> connRegistryBuilder = RegistryBuilder.create();
-	connRegistryBuilder.register("http", PlainConnectionSocketFactory.INSTANCE);
-	if (add.toLowerCase().startsWith("https://")) {
-	    try {
-		SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, new TrustStrategy() {
-		    @Override
-		    public boolean isTrusted(final X509Certificate[] chain, String authType) {
-			return true;
-		    }
-		}).build();
-		SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext,
-			SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
-		connRegistryBuilder.register("https", sslsf);
-	    } catch (Exception e) {
-		logger.warn("Exception thrown while trying to register https");
-		logger.debug("Stacktrace", e);
-	    }
+		RegistryBuilder<ConnectionSocketFactory> connRegistryBuilder = RegistryBuilder.create();
+		connRegistryBuilder.register("http", PlainConnectionSocketFactory.INSTANCE);
+		if (add.toLowerCase().startsWith("https://")) {
+			try {
+				SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, new TrustStrategy() {
+					@Override
+					public boolean isTrusted(final X509Certificate[] chain, String authType) {
+						return true;
+					}
+				}).build();
+				SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext,
+						SSLConnectionSocketFactory.ALLOW_ALL_HOSTNAME_VERIFIER);
+				connRegistryBuilder.register("https", sslsf);
+			} catch (Exception e) {
+				logger.warn("Exception thrown while trying to register https");
+				logger.debug("Stacktrace", e);
+			}
+		}
+		Registry<ConnectionSocketFactory> connRegistry = connRegistryBuilder.build();
+		PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(connRegistry);
+		connectionManager.setMaxTotal(10);
+		connectionManager.setDefaultMaxPerRoute(10);
+
+		HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+		clientBuilder.setDefaultRequestConfig(requestConfig);
+		clientBuilder.setConnectionManager(connectionManager);
+		clientBuilder.setUserAgent(CrawlerConstants.PROPERTIES.getProperty("crawler.userAgentString",
+				"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0"));
+
+		CloseableHttpClient httpClient = clientBuilder.setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
+
+		if (data.length == 3 && null != data[1] && !data[1].trim().isEmpty()) {
+			CredentialsProvider credsProvider = new BasicCredentialsProvider();
+			credsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(data[1], data[2]));
+			httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+		}
+
+		HttpGet get = new HttpGet(add);
+		CloseableHttpResponse response = httpClient.execute(new HttpGet(add));
+
+		get.releaseConnection();
+
+		// connectionManager.close();
+		return response;
 	}
-	Registry<ConnectionSocketFactory> connRegistry = connRegistryBuilder.build();
-	PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager(connRegistry);
-	connectionManager.setMaxTotal(10);
-	connectionManager.setDefaultMaxPerRoute(10);
 
-	HttpClientBuilder clientBuilder = HttpClientBuilder.create();
-	clientBuilder.setDefaultRequestConfig(requestConfig);
-	clientBuilder.setConnectionManager(connectionManager);
-	clientBuilder.setUserAgent(CrawlerConstants.PROPERTIES.getProperty("crawler.userAgentString",
-		"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0"));
-
-	CloseableHttpClient httpClient = clientBuilder.setSSLHostnameVerifier(new NoopHostnameVerifier()).build();
-
-	if (data.length == 3 && null != data[1] && !data[1].trim().isEmpty()) {
-	    CredentialsProvider credsProvider = new BasicCredentialsProvider();
-	    credsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(data[1], data[2]));
-	    httpClient = HttpClients.custom().setDefaultCredentialsProvider(credsProvider).build();
+	public static HttpResponse getUrlFluentResponse(String... data)
+			throws ParseException, ClientProtocolException, IOException {
+		String add = URLCanonicalizer.getCanonicalURL(data[0]);
+		Request request = Request.Get(add)
+				.addHeader("user-agent",
+						CrawlerConstants.PROPERTIES.getProperty("crawler.userAgentString",
+								"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0"))
+				.connectTimeout(Integer
+						.parseInt(CrawlerConstants.PROPERTIES.getProperty("crawler.connectionTimeout", "120000")))
+				.socketTimeout(Integer
+						.parseInt(CrawlerConstants.PROPERTIES.getProperty("crawler.connectionTimeout", "120000")));
+		if (data.length > 1 && null != data[1] && !data[1].trim().isEmpty()) {
+			String login = data[1] + ":" + data[2];
+			String base64login = new String(Base64.encodeBase64(login.getBytes()));
+			request.addHeader("Authorization", "Basic " + base64login);
+		}
+		return request.execute().returnResponse();
 	}
 
-	HttpGet get = new HttpGet(add);
-	CloseableHttpResponse response = httpClient.execute(new HttpGet(add));
+	public static JSONObject getStructuredData(String html, String url) throws SEOException {
+		CloseableHttpClient client = HttpClients.createDefault();
+		JSONObject json = null;
+		try {
+			HttpClientContext localContext = HttpClientContext.create();
+			HttpPost httpPost = new HttpPost(CrawlerConstants.PAGE_STRUCTURE_URL);
+			httpPost.addHeader("user-agent", CrawlerConstants.USER_AGENT);
+			httpPost.addHeader("referer", CrawlerConstants.PAGE_STRUCTURE_URL);
+			httpPost.addHeader("accept-encoding", "gzip, deflate, br");
+			httpPost.addHeader("accept-language", "en-US,en;q=0.8");
+			httpPost.addHeader("x-client-data", "CIW2yQEIpLbJAQjBtskB");
+			httpPost.addHeader("origin", "https://search.google.com");
 
-	get.releaseConnection();
-
-	// connectionManager.close();
-	return response;
-    }
-
-    public static HttpResponse getUrlFluentResponse(String... data)
-	    throws ParseException, ClientProtocolException, IOException {
-	String add = URLCanonicalizer.getCanonicalURL(data[0]);
-	Request request = Request.Get(add)
-		.addHeader("user-agent",
-			CrawlerConstants.PROPERTIES.getProperty("crawler.userAgentString",
-				"Mozilla/5.0 (Windows NT 10.0; WOW64; rv:48.0) Gecko/20100101 Firefox/48.0"))
-		.connectTimeout(
-			Integer.parseInt(CrawlerConstants.PROPERTIES.getProperty("crawler.connectionTimeout", "120000")))
-		.socketTimeout(
-			Integer.parseInt(CrawlerConstants.PROPERTIES.getProperty("crawler.connectionTimeout", "120000")));
-	if (data.length > 1 && null != data[1] && !data[1].trim().isEmpty()) {
-	    String login = data[1] + ":" + data[2];
-	    String base64login = new String(Base64.encodeBase64(login.getBytes()));
-	    request.addHeader("Authorization", "Basic " + base64login);
+			List<NameValuePair> params = new ArrayList<>();
+			params.add(new BasicNameValuePair("html", html));
+			httpPost.setEntity(new UrlEncodedFormEntity(params));
+			CloseableHttpResponse response = client.execute(httpPost, localContext);
+			String str = EntityUtils.toString(response.getEntity(), "UTF-8");
+			EntityUtils.consumeQuietly(response.getEntity());
+			httpPost.releaseConnection();
+			client.close();
+			json = new JSONObject(str.substring(4, str.length()));
+			json.remove("html");
+			json.remove("cse");
+		} catch (IOException e) {
+			logger.debug("Unable to fatch structured data", e);
+		}
+		if (json == null) {
+			throw new SEOException("Unable to fatch page structured data. url: " + url);
+		}
+		return json;
 	}
-	return request.execute().returnResponse();
-    }
 
-    public static JSONObject getStructuredData(String html, String url) throws SEOException {
-	CloseableHttpClient client = HttpClients.createDefault();
-	JSONObject json = null;
-	try {
-	    HttpClientContext localContext = HttpClientContext.create();
-	    HttpPost httpPost = new HttpPost(CrawlerConstants.PAGE_STRUCTURE_URL);
-	    httpPost.addHeader("user-agent", CrawlerConstants.USER_AGENT);
-	    httpPost.addHeader("referer", CrawlerConstants.PAGE_STRUCTURE_URL);
-	    httpPost.addHeader("accept-encoding", "gzip, deflate, br");
-	    httpPost.addHeader("accept-language", "en-US,en;q=0.8");
-	    httpPost.addHeader("x-client-data", "CIW2yQEIpLbJAQjBtskB");
-	    httpPost.addHeader("origin", "https://search.google.com");
+	public static JSONObject getPageSpeedData(String url, String strategy) throws SEOException {
+		JSONObject json = null;
+		try {
+			CloseableHttpClient client = HttpClients.createDefault();
+			URIBuilder builder = new URIBuilder(CrawlerConstants.PAGE_SPEED_URL);
+			builder.addParameter("key", CrawlerConstants.PAGE_SPEED_KEY);
+			builder.addParameter("locale", "en_US");
+			builder.addParameter("url", url);
+			builder.addParameter("strategy", strategy);
 
-	    List<NameValuePair> params = new ArrayList<>();
-	    params.add(new BasicNameValuePair("html", html));
-	    httpPost.setEntity(new UrlEncodedFormEntity(params));
-	    CloseableHttpResponse response = client.execute(httpPost, localContext);
-	    String str = EntityUtils.toString(response.getEntity(), "UTF-8");
-	    EntityUtils.consumeQuietly(response.getEntity());
-	    httpPost.releaseConnection();
-	    client.close();
-	    json = new JSONObject(str.substring(4, str.length()));
-	    json.remove("html");
-	    json.remove("cse");
-	} catch (IOException e) {
-	    logger.debug("Unable to fatch structured data", e);
+			HttpGet httpget = new HttpGet(builder.build());
+			httpget.addHeader("user-agent", CrawlerConstants.USER_AGENT);
+			CloseableHttpResponse response = client.execute(httpget);
+			String str = EntityUtils.toString(response.getEntity(), "UTF-8");
+			EntityUtils.consumeQuietly(response.getEntity());
+			httpget.releaseConnection();
+			client.close();
+			json = new JSONObject(str);
+			if (json.has("responseCode") && json.getInt("responseCode") != 200) {
+				throw new SEOException("Google Page Speed Insight API is throwing error, Response code: "
+						+ json.getInt("responseCode"));
+			} else {
+				json.remove("version");
+				json.remove("title");
+				json.remove("snapshots");
+				json.remove("screenshot");
+				json.remove("kind");
+				json.remove("captchaResult");
+				json.remove("id");
+			}
+
+		} catch (URISyntaxException | IOException e) {
+			logger.debug("Unable to fatch page speed data", e);
+		}
+		if (json == null) {
+			throw new SEOException("Unable to fatch page speed data. url: " + url);
+		}
+		return json;
 	}
-	if (json == null) {
-	    throw new SEOException("Unable to fatch page structured data. url: " + url);
-	}
-	return json;
-    }
 
-    public static JSONObject getPageSpeedData(String url, String strategy) throws SEOException {
-	JSONObject json = null;
-	try {
-	    CloseableHttpClient client = HttpClients.createDefault();
-	    URIBuilder builder = new URIBuilder(CrawlerConstants.PAGE_SPEED_URL);
-	    builder.addParameter("key", CrawlerConstants.PAGE_SPEED_KEY);
-	    builder.addParameter("locale", "en_US");
-	    builder.addParameter("url", url);
-	    builder.addParameter("strategy", strategy);
-
-	    HttpGet httpget = new HttpGet(builder.build());
-	    httpget.addHeader("user-agent", CrawlerConstants.USER_AGENT);
-	    CloseableHttpResponse response = client.execute(httpget);
-	    String str = EntityUtils.toString(response.getEntity(), "UTF-8");
-	    EntityUtils.consumeQuietly(response.getEntity());
-	    httpget.releaseConnection();
-	    client.close();
-	    json = new JSONObject(str);
-	    if (json.getInt("responseCode") != 200) {
-		throw new SEOException("Google Page Speed Insight API is throwing error, Response code: "
-			+ json.getInt("responseCode"));
-	    }
-	    json.remove("version");
-	    json.remove("title");
-	    json.remove("snapshots");
-	    json.remove("screenshot");
-	    json.remove("kind");
-	    json.remove("captchaResult");
-	    json.remove("id");
-	} catch (URISyntaxException | IOException e) {
-	    logger.debug("Unable to fatch page speed data", e);
+	public static boolean getWebUrlType(WebURL curURL, String host) {
+		Matcher m = CrawlerConstants.PATTERN.matcher(curURL.getURL());
+		String prop = CrawlerConstants.PROPERTIES.getProperty("crawler.domainRegex");
+		if (null == prop || prop.isEmpty() || prop.equals(".")) {
+			return Util.getUrlHost(curURL.getURL()).contains(host);
+		} else {
+			if (!m.find()) {
+				return false;
+			} else {
+				return true;
+			}
+		}
 	}
-	if (json == null) {
-	    throw new SEOException("Unable to fatch page speed data. url: " + url);
-	}
-	return json;
-    }
-
 }
