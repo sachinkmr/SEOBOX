216e95bb2122c0b28284ce504396680219e755ed
Sachin <sachinkmr3112@gmail.com>
2017-02-07 15:34:58 +0530

    jenkins interface logger

--- src/main/java/sachin/seobox/parameters/PageSpeed.java
+++ src/main/java/sachin/seobox/parameters/PageSpeed.java
@@ -3,19 +3,24 @@ package sachin.seobox.parameters;
 import sachin.seobox.helpers.DBUtils;
 
 public class PageSpeed implements Runnable {
-    private final String url;
+	private final String url;
 
-    public PageSpeed(String url) {
-	this.url = url;
-    }
+	public PageSpeed(String url) {
+		this.url = url;
+	}
 
-    public String getUrl() {
-	return this.url;
-    }
+	public String getUrl() {
+		return this.url;
+	}
 
-    @Override
-    public void run() {
-	DBUtils.insertPageSpeedRecord(url);
-    }
+	@Override
+	public void run() {
+		DBUtils.insertPageSpeedRecord(url);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+
+		}
+	}
 
 }
--- src/main/java/sachin/seobox/parameters/StructuredData.java
+++ src/main/java/sachin/seobox/parameters/StructuredData.java
@@ -3,25 +3,30 @@ package sachin.seobox.parameters;
 import sachin.seobox.helpers.DBUtils;
 
 public class StructuredData implements Runnable {
-    private final String url;
-    private final String html;
+	private final String url;
+	private final String html;
 
-    public StructuredData(String url, String html) {
-	this.url = url;
-	this.html = html;
-    }
+	public StructuredData(String url, String html) {
+		this.url = url;
+		this.html = html;
+	}
 
-    public String getUrl() {
-	return this.url;
-    }
+	public String getUrl() {
+		return this.url;
+	}
 
-    public String getHtml() {
-	return this.html;
-    }
+	public String getHtml() {
+		return this.html;
+	}
 
-    @Override
-    public void run() {
-	DBUtils.insertStructuredDataRecord(url, html);
-    }
+	@Override
+	public void run() {
+		DBUtils.insertStructuredDataRecord(url, html);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+
+		}
+	}
 
 }
