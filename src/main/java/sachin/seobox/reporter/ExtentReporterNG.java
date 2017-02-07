8bea306182ee3918c0820348f4eb2eda8c04d497
Sachin <sachinkmr3112@gmail.com>
2017-02-07 15:17:12 +0530

    jenkins interface

--- src/main/java/sachin/seobox/reporter/ExtentReporterNG.java
+++ src/main/java/sachin/seobox/reporter/ExtentReporterNG.java
@@ -13,37 +13,38 @@ import org.jsoup.nodes.Element;
 import sachin.seobox.crawler.CrawlerConstants;
 
 public class ExtentReporterNG {
-    public static void generateReport() {
-	System.out.println("\nGenerating Report, Please Wait.....");
-	System.out.println("----------------------------------------------------------");
-	ComplexReportFactory.getInstance().closeReport();
-	if (CrawlerConstants.ERROR && !CrawlerConstants.ERROR_TEXT.isEmpty()) {
-	    try {
-		File file = new File(CrawlerConstants.REPORT_PATH);
-		Document doc = Jsoup.parse(file, "UTF-8");
-		Element e = doc.select("div.loading").first();
-		e.attr("style", "display:block");
-		String errorText = "URL: <a href='" + CrawlerConstants.SITE + "'>" + CrawlerConstants.SITE + "</a><br/>"
-			+ CrawlerConstants.ERROR_TEXT;
-		e.select("#error").first().html(errorText);
-		FileUtils.writeStringToFile(file, doc.outerHtml(), "UTF-8");
-	    } catch (IOException e) {
-	    }
-	}
-	if ((null != System.getProperty("JENKINS") && !System.getProperty("JENKINS").isEmpty())
-		|| (null != System.getProperty("WEB") && !System.getProperty("WEB").isEmpty())) {
-	    String path = CrawlerConstants.REPORT_PATH.substring(0, CrawlerConstants.REPORT_PATH.indexOf("webapps\\") + 8);
-	    try {
-		System.out.println("Report Generated: "
-			+ CrawlerConstants.REPORT_PATH.replace(path, "http://" + InetAddress.getLocalHost().getHostAddress()));
-	    } catch (UnknownHostException e) {
+	public static void generateReport() {
+		System.out.println("\nGenerating Report, Please Wait.....");
+		System.out.println("----------------------------------------------------------");
+		ComplexReportFactory.getInstance().closeReport();
+		if (CrawlerConstants.ERROR && !CrawlerConstants.ERROR_TEXT.isEmpty()) {
+			try {
+				File file = new File(CrawlerConstants.REPORT_PATH);
+				Document doc = Jsoup.parse(file, "UTF-8");
+				Element e = doc.select("div.loading").first();
+				e.attr("style", "display:block");
+				String errorText = "URL: <a href='" + CrawlerConstants.SITE + "'>" + CrawlerConstants.SITE + "</a><br/>"
+						+ CrawlerConstants.ERROR_TEXT;
+				e.select("#error").first().html(errorText);
+				FileUtils.writeStringToFile(file, doc.outerHtml(), "UTF-8");
+			} catch (IOException e) {
+			}
+		}
+		if ((null != System.getProperty("JENKINS_URL") && !System.getProperty("JENKINS_URL").isEmpty())
+				|| (null != System.getProperty("WEB") && !System.getProperty("WEB").isEmpty())) {
+			String path = CrawlerConstants.REPORT_PATH.substring(0,
+					CrawlerConstants.REPORT_PATH.indexOf("webapps\\") + 8);
+			try {
+				System.out.println("Report Generated: " + CrawlerConstants.REPORT_PATH.replace(path,
+						"http://" + InetAddress.getLocalHost().getHostAddress()));
+			} catch (UnknownHostException e) {
 
-	    }
-	    FileUtils.deleteQuietly(new File(System.getProperty("user.dir"), "Config.properties"));
-	    FileUtils.deleteQuietly(new File(System.getProperty("user.dir"), "CrawlerConfigFile"));
-	} else {
-	    System.out.println("Report Generated: " + CrawlerConstants.REPORT_PATH);
-	}
+			}
+			FileUtils.deleteQuietly(new File(System.getProperty("user.dir"), "Config.properties"));
+			FileUtils.deleteQuietly(new File(System.getProperty("user.dir"), "CrawlerConfigFile"));
+		} else {
+			System.out.println("Report Generated: " + CrawlerConstants.REPORT_PATH);
+		}
 
-    }
+	}
 }
